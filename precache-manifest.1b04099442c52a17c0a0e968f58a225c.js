618eaf0acdff1c6a1fcd6cfabf44775b32d43f3b
Your Name <you@example.com>
2019-09-17 10:29:25 +0200

    Updates

---
+++ precache-manifest.1b04099442c52a17c0a0e968f58a225c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d94207082d5deb7039d8db9ec3d1440",
+    "url": "/React-map-snazzy-with-search/index.html"
+  },
+  {
+    "revision": "4cd59c64283a41955c82",
+    "url": "/React-map-snazzy-with-search/static/js/2.e9497487.chunk.js"
+  },
+  {
+    "revision": "33c25daea48348b788f4",
+    "url": "/React-map-snazzy-with-search/static/js/main.b42b7c01.chunk.js"
+  },
+  {
+    "revision": "8f5e069bae79b9276f8e",
+    "url": "/React-map-snazzy-with-search/static/js/runtime~main.dd23c8a7.js"
+  }
+]);
